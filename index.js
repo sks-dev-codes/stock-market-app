7a1bf9a3527126a790c0bb99224bfe7e6172bb7e
Saurabh Sharma <saurabh.sharma232@outlook.com>
2024-09-16 07:36:15 +0000

    added express-handlebars to handle dynamic stuffs..

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars')
 const path = require('path');
 
 const PORT = process.env.port || 5000;
 
+//set handlebars middleware
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
 
+const otherStuff = "hello there, this is other stuff!"
+//set handlebars routes
+app.get('/', function (req, res) {
+    res.render('home', { stuff: otherStuff });
+});
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.listen(PORT, () => console.log('server listening on port '+ PORT));
+app.listen(PORT, () => console.log('server listening on port ' + PORT));
