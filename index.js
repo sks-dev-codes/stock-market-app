001685ae3d29fbb7e4d550d88db74541d0c3c58f
Saurabh Sharma <saurabh.sharma232@outlook.com>
2024-09-17 14:21:34 +0000

    tested post method api call

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
 const request = require('request');
+const bodyParser = require('body-parser');
 const options = { url: "https://alpha-vantage.p.rapidapi.com/query?datatype=json&keywords=microsoft&function=SYMBOL_SEARCH", headers: { 'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com', 'x-rapidapi-key': 'ea0ce447d8msh2731dc4bc559762p17c644jsndc4e53168ab7' }, json: true };
 
 const PORT = process.env.port || 5000;
 
+//use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+
 //create call_api function
-function call_api(finishedAPI) {
+function call_api(finishedAPI, ticker) {
+    var uri = "https://alpha-vantage.p.rapidapi.com/query?datatype=json&keywords="+ticker+"&function=SYMBOL_SEARCH"
+    options.url = uri
     request(options, (error, res, body) => {
         if (error) {
             console.error('Error making request:', error);
@@ -28,12 +34,20 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 const otherStuff = "hello there, this is other stuff!";
-//set handlebars routes
+//set handlebars index route
 app.get('/', function (req, res) {
     call_api(function (doneAPI) {
         res.render('home', { stock: doneAPI });
-    });
+    }, "goog");
 });
+
+//set handlebars index POST route
+app.post('/', function (req, res) {
+    call_api(function (doneAPI) {
+        res.render('home', { stock: doneAPI });
+    }, req.body.stock_ticker);
+});
+
 
 //create about page route
 app.get('/about.html', function (req, res) {
